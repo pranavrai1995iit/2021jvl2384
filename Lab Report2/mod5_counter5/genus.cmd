effb276472a06a0fff24a9e1f87da1495f69746a
pranavrai1995iit <89929083+pranavrai1995iit@users.noreply.github.com>
2021-10-17 22:18:14 +0530

    Add files via upload

---
+++ Lab Report2/mod5_counter5/genus.cmd
@@ -0,0 +1,9 @@
+# Cadence Genus(TM) Synthesis Solution, Version 19.12-s121_1, built Dec  3 2019 15:07:17
+
+# Date: Fri Oct 15 21:53:48 2021
+# Host: legendre1 (x86_64 w/Linux 4.9.0-4-amd64) (1core*18cpus*1physical cpu*Intel(R) Xeon(R) CPU E5-2650 v2 @ 2.60GHz 20480KB)
+# OS:   Unsupported OS as /etc does not have release info
+
+source ./synthesis/mod5_counter.tcl
+show_gui
+gui_show
